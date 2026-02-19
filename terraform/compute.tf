8098c73ed9b76ce41e64b3194589651b73e773bd
Damien-Mrgnc <157527343+Damien-Mrgnc@users.noreply.github.com>
2026-02-19 13:56:21 +0100

    fix: Remove Role Assignment due to insufficient permissions, rely on Admin Creds

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -62,9 +62,3 @@ resource "azurerm_linux_web_app" "main" {
 
   tags = local.tags
 }
-
-resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.main.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_web_app.main.identity[0].principal_id
-}
